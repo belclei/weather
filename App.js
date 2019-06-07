7f21f6ea46f25e543c331ffea81a2946efda160d
belclei <belclei@gmail.com>
2019-06-06 22:57:18 -0300

    foram criados states para cada informação dinamica. e também foi integrado o app com o backend

--- App.js
+++ App.js
@@ -5,41 +5,95 @@ import {
   KeyboardAvoidingView,
   Platform,
   ImageBackground,
-  View
+  View,
+  ActivityIndicator,
+  StatusBar
 } from "react-native";
 
 import SearchInput from "./components/SearchInput";
+
+import { fetchLocationId, fetchWeather } from "./utils/api";
 import getImageForWeather from "./utils/getImageForWeather";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: "Viamão"
+      location: "",
+      loading: false,
+      error: false,
+      temperature: 0,
+      weather: ""
     };
   }
-  handleUpdateLocation = city => {
-    this.setState({ location: city });
+  handleUpdateLocation = async city => {
+    if (!city) return;
+
+    this.setState({ loading: true }, async () => {
+      try {
+        const locationId = await fetchLocationId(city);
+        const { location, weather, temperature } = await fetchWeather(
+          locationId
+        );
+
+        this.setState({
+          loading: false,
+          error: false,
+          location,
+          weather,
+          temperature
+        });
+      } catch (e) {
+        this.setState({
+          loading: false,
+          error: true
+        });
+      }
+    });
   };
+  componentDidMount() {
+    this.handleUpdateLocation("Shanghai");
+  }
   render() {
+    const { loading, error, location, weather, temperature } = this.state;
+
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <StatusBar barStyle="light-content" />
         <ImageBackground
-          source={getImageForWeather("Clear")}
+          source={getImageForWeather(weather)}
           style={styles.imageContainer}
           imageStyle={styles.image}
         >
           <View style={styles.detailsContainer}>
-            <Text style={[styles.largeText, styles.textStyle]}>
-              {this.state.location}
-            </Text>
-            <Text style={[styles.smallText, styles.textStyle]}>Light Cold</Text>
-            <Text style={[styles.largeText, styles.textStyle]}>24º</Text>
-
-            <SearchInput
-              placeholder="Informe a cidade desejada"
-              onSubmit={this.handleUpdateLocation}
-            />
+            <ActivityIndicator animating={loading} color="white" size="large" />
+            {!loading && (
+              <View>
+                {error && (
+                  <Text style={[styles.smallText, styles.textStyle]}>
+                    Não foi possivel descobrir como está o clima nesta região
+                    pesquisada
+                  </Text>
+                )}
+                {!error && (
+                  <View>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {location}
+                    </Text>
+                    <Text style={[styles.smallText, styles.textStyle]}>
+                      {weather}
+                    </Text>
+                    <Text
+                      style={[styles.largeText, styles.textStyle]}
+                    >{`${Math.round(temperature)}º`}</Text>
+                  </View>
+                )}
+                <SearchInput
+                  placeholder="Informe a cidade desejada"
+                  onSubmit={this.handleUpdateLocation}
+                />
+              </View>
+            )}
           </View>
         </ImageBackground>
       </KeyboardAvoidingView>
